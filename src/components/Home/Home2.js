bb1585298da5a70c8f691851542f1924da4a2132
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2022-06-29 08:36:01 -0400

    fixed wechat tooltip mobile view

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -2,118 +2,147 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headshot from "../../Assets/me.png";
 import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub,
-  AiFillWechat
-} from "react-icons/ai";
+import { AiFillGithub, AiFillWechat } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import ReactTooltip from "react-tooltip";
 import wechatContact from "../../Assets/wechatContact.JPG";
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
+import { createMedia } from "@artsy/fresnel";
+
+const { MediaContextProvider, Media } = createMedia({
+  breakpoints: {
+    sm: 375,
+    md: 768,
+    lg: 1024,
+    xl: 1192,
+  },
+});
 
 function Home2() {
   return (
     <Container fluid className="home-about-section" id="about">
       <Container>
-      <Fade bottom>
-        <Row>
-          <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
-            </h1>
-            <p className="home-about-body">
-            I’m currently pursuing my Bachelor of Software Engineering degree at <i><b className="purple">Western University, Canada</b></i> 
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> Javascript, HTML, CSS and Java. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Full Stack Web & Mobile Technologies and Products </b>
-                </i> and
-                also in areas related to{" "}
+        <Fade bottom>
+          <Row>
+            <Col md={8} className="home-about-description">
+              <h1 style={{ fontSize: "2.6em" }}>
+                LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              </h1>
+              <p className="home-about-body">
+                I’m currently pursuing my Bachelor of Software Engineering
+                degree at{" "}
                 <i>
-                <b className="purple">
-                  Business Analytics.
-                </b>
+                  <b className="purple">Western University, Canada</b>
                 </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <i><b className="purple">Node.js</b></i> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Angular.js</b>
-              </i>
-            </p>
-          </Col>
-          <Col md={4} className="myAvtar">
-            <Tilt>
-              <div className="myAvtarContainer">
-                <img src={headshot} className="img-fluid" alt="avatar" />
-              </div>
-            </Tilt>
-          </Col>
-        </Row>
-      </Fade>
-      <Fade bottom>
-        <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="https://github.com/HenryCZhang"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/henry-zhang-uwo/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <div
-                  href="weixin://dl/chat?henry137589"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillWechat data-tip data-for="wechat"/>
+                <br />
+                <br />I am fluent in classics like
+                <i>
+                  <b className="purple"> Javascript, HTML, CSS and Java. </b>
+                </i>
+                <br />
+                <br />
+                My field of Interest's are building new &nbsp;
+                <i>
+                  <b className="purple">
+                    Full Stack Web & Mobile Technologies and Products{" "}
+                  </b>
+                </i>{" "}
+                and also in areas related to{" "}
+                <i>
+                  <b className="purple">Business Analytics.</b>
+                </i>
+                <br />
+                <br />
+                Whenever possible, I also apply my passion for developing
+                products with{" "}
+                <i>
+                  <b className="purple">Node.js</b>
+                </i>{" "}
+                and
+                <i>
+                  <b className="purple">
+                    {" "}
+                    Modern Javascript Library and Frameworks
+                  </b>
+                </i>
+                &nbsp; like
+                <i>
+                  <b className="purple"> React.js and Angular.js</b>
+                </i>
+              </p>
+            </Col>
+            <Col md={4} className="myAvtar">
+              <Tilt>
+                <div className="myAvtarContainer">
+                  <img src={headshot} className="img-fluid" alt="avatar" />
                 </div>
-              </li>
-              <ReactTooltip
-              id="wechat"
-              fontSize="large"
-              backgroundColor="none"
-              place="bottom"
-            >
-              <img style={{width:"15rem"}} src={wechatContact} />
-            </ReactTooltip>
-            </ul>
-          </Col>
-        </Row>
+              </Tilt>
+            </Col>
+          </Row>
+        </Fade>
+        <Fade bottom>
+          <Row>
+            <Col md={12} className="home-about-social">
+              <h1>FIND ME ON</h1>
+              <p>
+                Feel free to <span className="purple">connect </span>with me
+              </p>
+              <ul className="home-about-social-links">
+                <li className="social-icons">
+                  <a
+                    href="https://github.com/HenryCZhang"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <AiFillGithub />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://www.linkedin.com/in/henry-zhang-uwo/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <div
+                    href="weixin://dl/chat?henry137589"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <AiFillWechat data-tip data-for="wechat" />
+                  </div>
+                </li>
+                <MediaContextProvider>
+                  <Media greaterThan="sm">
+                    <ReactTooltip
+                      id="wechat"
+                      fontSize="large"
+                      backgroundColor="none"
+                      place="bottom"
+                    >
+                      <img style={{ width: "15rem" }} src={wechatContact} />
+                    </ReactTooltip>
+                  </Media>
+                  <Media lessThan="lg">
+                    <ReactTooltip
+                      id="wechat"
+                      fontSize="large"
+                      backgroundColor="none"
+                      place="top"
+                    >
+                      <img style={{ width: "15rem" }} src={wechatContact} />
+                    </ReactTooltip>
+                  </Media>
+                </MediaContextProvider>
+              </ul>
+            </Col>
+          </Row>
         </Fade>
       </Container>
     </Container>
